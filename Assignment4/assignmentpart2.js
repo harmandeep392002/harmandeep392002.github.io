431664a49075f2609dba03cfe9aeea50257e8003
harmandeep392002 <harmandeep3@dcmail.ca>
2024-08-01 12:35:06 -0400

    array and object created

--- Assignment4/assignmentpart2.js
+++ Assignment4/assignmentpart2.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageName = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altName = {'pic1.jpg': 'Human Eye', 'pic2.jpg': 'Rock', 'pic3.jpg': 'Blue and White flowers', 'pic4.jpg': 'Drawing', 'pic5.jpg': 'Butterfly'};
 
 /* Looping through images */
 
