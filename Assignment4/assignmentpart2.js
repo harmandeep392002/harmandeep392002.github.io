85edaaaee2bed52a5786cc30d450e22afb451a38
harmandeep392002 <harmandeep3@dcmail.ca>
2024-08-01 12:46:30 -0400

    wiring button

--- Assignment4/assignmentpart2.js
+++ Assignment4/assignmentpart2.js
@@ -25,5 +25,19 @@ for(image of imageName){
 };
 
 
-
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', function(){
+    let presentClass = btn.getAttribute('class');
+    if(presentClass == 'dark'){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor =" rgb(0 0 0 / 50%)";
+    }
+
+    else{
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor =" rgb(0 0 0 / 0%)";
+    }
+})
