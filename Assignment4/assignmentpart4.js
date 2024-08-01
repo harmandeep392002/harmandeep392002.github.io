83331b58883b186f8c55c9d94b5afbf1f0b4eabd
harmandeep392002 <harmandeep3@dcmail.ca>
2024-08-01 13:08:17 -0400

    Shape class created and ball class updated

--- Assignment4/assignmentpart4.js
+++ Assignment4/assignmentpart4.js
@@ -18,14 +18,24 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
+class Shape {
+
+    constructor(x, y, velX, velY) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+    }
+  
+}
+
+class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
+    
   }
 
   draw() {
@@ -58,7 +68,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
